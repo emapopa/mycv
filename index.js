3c526133aa148656413b78baf71ee62b82b1e0ba
Emanuel Popa <54033152+emapopa@users.noreply.github.com>
2023-02-02 15:52:11 +0100

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,53 @@
-// Import stylesheets
-import './style.css';
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+hamburger.addEventListener('click', ()=>{
+   //Animate Links
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+
+    //Hamburger Animation
+    hamburger.classList.toggle("toggle");
+});
+
+// set up text to print, each item in array is new line
+let aText = new Array(
+    "Hello world, my name is Emanuel ", 
+    "Im a Ux and Ui designer with a background in frontend "
+    );
+    let iSpeed = 100; // time delay of print out
+    let iIndex = 0; // start printing array at this posision
+    let iArrLength = aText[0].length; // the length of the text array
+    let iScrollAt = 20; // start scrolling up at this many lines
+     
+    let iTextPos = 0; // initialise text position
+    let sContents = ''; // initialise contents variable
+    let iRow; // initialise current row
+     
+    function typewriter()
+    {
+     sContents =  ' ';
+     iRow = Math.max(0, iIndex-iScrollAt);
+     let destination = document.getElementById("typedtext");
+     
+     while ( iRow < iIndex ) {
+      sContents += aText[iRow++] + '<br />';
+     }
+     destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+     if ( iTextPos++ == iArrLength ) {
+      iTextPos = 0;
+      iIndex++;
+      if ( iIndex != aText.length ) {
+       iArrLength = aText[iIndex].length;
+       setTimeout("typewriter()", 500);
+      }
+     } else {
+      setTimeout("typewriter()", iSpeed);
+     }
+    }
+    
+    
+    typewriter();
