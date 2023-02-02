a8312fa0a799bcb772c75b40531be6f2d7917bf5
emapopa <emapopa1995@gmail>
2023-02-02 16:42:18 +0100

    added modal functionality

--- index.js
+++ index.js
@@ -27,27 +27,3 @@ let aText = new Array(
     let sContents = ''; // initialise contents variable
     let iRow; // initialise current row
      
-    function typewriter()
-    {
-     sContents =  ' ';
-     iRow = Math.max(0, iIndex-iScrollAt);
-     let destination = document.getElementById("typedtext");
-     
-     while ( iRow < iIndex ) {
-      sContents += aText[iRow++] + '<br />';
-     }
-     destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
-     if ( iTextPos++ == iArrLength ) {
-      iTextPos = 0;
-      iIndex++;
-      if ( iIndex != aText.length ) {
-       iArrLength = aText[iIndex].length;
-       setTimeout("typewriter()", 500);
-      }
-     } else {
-      setTimeout("typewriter()", iSpeed);
-     }
-    }
-    
-    
-    typewriter();
